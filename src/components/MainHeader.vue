f75232951c57c5782d5fb61e4907cd667d34ee5c
Nik Steinbrügge <ni851ste@htwg-konstanz.de>
2020-01-21 15:03:07 +0100

    Authentication base Commit

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -2,11 +2,27 @@
     <span class="main-header border border-primary col-lg-10 col-md-10">
         <p id="main-header-current-player">{{this.currentPlayerName}}</p>
         <p id="main-header-current-money">{{this.currentPlayerMoney}} €</p>
-                </span>
+    <template v-if="user.loggedIn">
+                        <div class="nav-item">{{user.data.displayName}}</div>
+                        <li class="nav-item">
+                            <a class="nav-link" @click.prevent="signOut">Sign out</a>
+                        </li>
+                    </template>
+                    <template v-else>
+                        <li class="nav-item">
+                            <router-link to="login" class="nav-link">Login</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link to="register" class="nav-link">Register</router-link>
+                        </li>
+                    </template>
+    </span>
 
 </template>
 
 <script>
+    import {mapGetters} from "vuex";
+    import firebase from "firebase";
 
     function getCurrentPlayer(json) {
         return json.board.players.find(p => p.name === json.board.current_player);
@@ -27,24 +43,40 @@
             currentPlayerMoney: Number,
             tmpMessage: String
         },
+        computed: {
+            ...mapGetters({
+// map `this.user` to `this.$store.getters.user`
+                user: "user"
+            })
+        },
         methods: {
             async demandUpdate() {
                 await new Promise(r => setTimeout(r, 300));
                 this.$socket.send('json');
             },
+            signOut() {
+                firebase
+                    .auth()
+                    .signOut()
+                    .then(() => {
+                        this.$router.replace({
+                            name: "Login"
+                        });
+                    });
 
-        },
-        beforeMount() {
-            this.demandUpdate()
-        },
-        created() {
-            this.$options.sockets.onmessage = (data) => {
-                let json = JSON.parse(data.data)
-                window.console.log("DATA");
-                window.console.log(json);
-                this.currentPlayerName = currentPlayerName(json);
-                this.currentPlayerMoney = getCurrentMoney(json);
-            };
+            },
+            beforeMount() {
+                this.demandUpdate()
+            },
+            created() {
+                this.$options.sockets.onmessage = (data) => {
+                    let json = JSON.parse(data.data)
+                    window.console.log("DATA");
+                    window.console.log(json);
+                    this.currentPlayerName = currentPlayerName(json);
+                    this.currentPlayerMoney = getCurrentMoney(json);
+                };
+            }
         }
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -8,7 +8,11 @@ import Game from './components/Game'
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 import './registerServiceWorker'
-
+import Login from "@/components/Login";
+import Register from "@/components/Register";
+import Dashboard from "@/components/Dashboard";
+import store from "./store";
+import * as firebase from "firebase";
 
 
 Vue.config.productionTip = false;
@@ -16,25 +20,53 @@ Vue.config.productionTip = false;
 Vue.use(UUID);
 Vue.use(VueRouter);
 Vue.use(VueNativeSock, 'ws://localhost:9000/websocket', {
-  reconnection: true, // (Boolean) whether to reconnect automatically (false)
-  reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
-  reconnectionDelay: 3000, // (Number) how long to initially wait before attempting a new (1000)
+    reconnection: true, // (Boolean) whether to reconnect automatically (false)
+    reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
+    reconnectionDelay: 3000, // (Number) how long to initially wait before attempting a new (1000)
 });
 Vue.use(Toast, {});
 
 
-const routes = [
-  {path: '/', component: Rules },
-  {path: '/game', component: Game },
-  //{path: '/rules', component: Rules }
-];
-
 const router = new VueRouter({
-  mode: 'history',
-  routes: routes
+    mode: 'history',
+    routes: [
+        {path: '/', component: Rules},
+
+        {
+            path: '/game', name: "Game", component: Game, beforeEnter: (to, from, next) => {
+                if (store.getters.loggedIn === false) {
+                    next("/login");
+                } else {
+                    next()
+                }
+            }
+        },
+        {path: '/rules', name: 'Rules', component: Rules},
+        {path: '/login', name: 'Login', component: Login},
+        {path: '/register', name: 'Register', component: Register},
+        {path: '/dashboard', name: 'Dashboard', component: Dashboard}
+    ]
 });
 
 new Vue({
-  router: router,
-  render: h => h(App)
+    router: router,
+    store,
+    render: h => h(App)
 }).$mount('#app');
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyANMAoY-smVMDj5AJJGJYMD6_97FdmEtWM",
+  authDomain: "htwg-webtech-rummy.firebaseapp.com",
+  databaseURL: "https://htwg-webtech-rummy.firebaseio.com",
+  projectId: "htwg-webtech-rummy",
+  storageBucket: "htwg-webtech-rummy.appspot.com",
+  messagingSenderId: "206658326580",
+  appId: "1:206658326580:web:4e6a7faf153d80f0243a61"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch("fetchUser", user);
+});
